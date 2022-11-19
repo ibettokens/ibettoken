296dc610c9b05429bc6067465f030b9417fb9689
Ibet Tokens <IbetTokens@protonmail.com>
2022-11-18 22:21:02 -0600

    fix eth as well

--- src/ibetapp/buildethdocker.sh
+++ src/ibetapp/buildethdocker.sh
@@ -1,3 +1,4 @@
  docker build -t powersvr:5000/cryptobets-ethereum -f Ethereum.Dockerfile .
+ docker push powersvr:5000/cryptobets-ethereum
  docker build -t powersvr:5000/cryptobets-matic -f Matic.Dockerfile .
  docker push powersvr:5000/cryptobets-matic
