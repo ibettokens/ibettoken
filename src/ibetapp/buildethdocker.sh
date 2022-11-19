c0d58069cca1b5faf415d6036b56b3c2397746b1
Ibet Tokens <IbetTokens@protonmail.com>
2022-11-18 21:31:12 -0600

    updated docker and angular

--- src/ibetapp/buildethdocker.sh
+++ src/ibetapp/buildethdocker.sh
@@ -1,1 +1,3 @@
  docker build -t powersvr:5000/cryptobets-ethereum -f Ethereum.Dockerfile .
+ docker build -t powersvr:5000/cryptobets-matic -f Matic.Dockerfile .
+ docker push powersvr:5000/cryptobets-matic
